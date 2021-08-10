5ce57b92e7b410f61e024e26fb0a2b03c1b2598f
yuruhi <yuruhiya@gmail.com>
2021-08-10 11:31:07 +0900

    refactor utility

--- src/utility.cr
+++ src/utility.cr
@@ -1,13 +1,21 @@
 require "colorize"
 
 module OIJ
+  private def self.put_message(title, color, message)
+    STDERR << '[' << title.colorize(color) << ']' << ' ' << message << '\n'
+  end
+
   def self.error(message)
-    STDERR.puts "[#{"ERROR".colorize(:red)}] #{message}"
+    put_message("ERROR", :red, message)
     exit(1)
   end
 
+  def self.warning(message)
+    put_message("WARING", :yellow, message)
+  end
+
   def self.info(message)
-    STDERR.puts "[#{"INFO".colorize(:blue)}] #{message}"
+    put_message("INFO", :blue, message)
   end
 
   def self.system(command : String)
